18f19966070074117e74ad12e488f591d932f385
kingyeonsu <yeonsu1254@naver.com>
2022-09-14 05:12:32 +0000

    First commit

---
+++ src/app/View/View.h
@@ -0,0 +1,27 @@
+#ifndef __VIEW_H__
+#define __VIEW_H__
+
+#include <string>
+#include "Led.h"
+#include "LightState.h"
+
+class View
+{
+private:
+    int lightState;
+    Led *light1, *light2, *light3, *light4, *light5;
+
+public:
+    View(Led *led1, Led *led2, Led *led3, Led *led4, Led *led5);
+    virtual ~View();
+    void setState(int state);
+    void lightView();
+    void lightOn_1();
+    void lightOn_2();
+    void lightOn_3();
+    void lightOn_4();
+    void lightOn_5();
+    void lightOff();
+};
+
+#endif /* __VIEW_H__ */
